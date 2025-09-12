0344228ff9262ecd1dfae236d283633774947fa2
Maik <maik.neubert@belegebox.de>
2025-09-12 13:25:37 +0200

    order map by cities

---
+++ collection-map/data.js
@@ -0,0 +1,71 @@
+var collectionPoints = [
+    {
+        "city": "Würzburg",
+        "postalCode": "97076",
+        "street": "Ohmstraße",
+        "house": "1",
+        "latLong": [49.7974, 9.9708],
+        "openingHours": {
+            "Sunday": ["12:00-18:00"]
+        },
+        "duration": "01.10 - 30.11",
+        "organization": "HERMINE e. V.",
+        "name": "HERMINE e. V. Lagerhalle"
+    },
+    {
+        "city": "Würzburg",
+        "latLong": [49.8008, 9.9717],
+        "name": "RockInn Boulderhalle"
+    },
+    {
+        "city": "Würzburg",
+        "latLong": [49.7951, 9.9405],
+        "name": "KHG"
+    },
+    {
+        "city": "Dortmund",
+        "latLong": [51.529124, 7.440845],
+        "name": "Grenzenlose Wärme e. V."
+    },
+    {
+        "city": "Biesenthal",
+        "latLong": [52.754285, 13.674776],
+        "name": "Wir packen's an e.V."
+    },
+]
+
+var cities = [
+    {
+        "name": "Würzburg",
+        "url": "wuerzburg",
+        "latLong": [49.79, 9.95]
+    },
+    {
+        "name": "Dortmund",
+        "url": "dortmund",
+        "latLong": [51.51, 7.46] 
+    },
+    {
+        "name": "Biesenthal",
+        "url": "biesenthal",
+        "latLong": [52.76, 13.64]
+    }
+]
+
+var organizations = [
+    {
+        "name": "HERMINE e. V.",
+        "cities": ["Würzburg"],
+        "supportedOrganisations": ["RWC", "SV", ""],
+    },
+    {
+        "name": "Grenzenlose Wärme e. V.",
+        "cities": ["Dortmund"],
+        "supportedOrganisations": ["RWC", "SV"],
+    },
+        {
+        "name": "Wir packen's an e.V.",
+        "cities": ["Biesenthal"],
+        "supportedOrganisations": ["RWC", "SV"],
+    },
+]
--- collection-map/map.js
+++ collection-map/map.js
@@ -1,26 +1,42 @@
-var collectionPointMarker = L.icon({
-    iconUrl: 'images/collection-point.svg',
+var urlParams = new URLSearchParams(window.location.search)
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+var collectionPointMarker = L.icon({
+    iconUrl: 'images/collection-point.avif',
+
+    iconSize:     [32, 48], // size of the icon
+    iconAnchor:   [16, 48], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -48] // point from which the popup should open relative to the iconAnchor
 });
 
+let viewerLocation = urlParams.get('location') ?? 'germany';
+
+let viewerCity = cities.find(city => city.url === viewerLocation);
+if (viewerLocation === 'germany' || viewerCity === undefined) {
+    viewCoordinates = [51.1, 10.34];
+    zoom = 6;
+} else {
+    viewCoordinates = viewerCity.latLong;
+    zoom = 13;
+}
 
 
-
-var map = L.map('map').setView([49.791, 9.953], 13);
+var map = L.map('map').setView(viewCoordinates, zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-for (collectionPoint of collectionPoints) {
-    let marker = L.marker(collectionPoint.latLong, { icon: collectionPointMarker }).addTo(map);
-    marker.bindPopup(`<b>${collectionPoint.organizedBy}</b>`);
 
+if (location === 'germany' || viewerCity === undefined) {
+    for (city of cities) {
+        let marker = L.marker(city.latLong, { icon: collectionPointMarker }).addTo(map);
+        marker.bindPopup(`<b>${city.name}</b><br><a href="index.html?location=${city.url}">Sammelstellen anzeigen</a>`);
+    }
+} else {
+    for (collectionPoint of collectionPoints) {
+        let marker = L.marker(collectionPoint.latLong, { icon: collectionPointMarker }).addTo(map);
+        marker.bindPopup(`<b>${collectionPoint.name}</b>`);
+    }
 }
-
