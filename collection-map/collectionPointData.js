20654bbcbf565facdbaeac5379c82bbffffeaa09
Maik <maik.neubert@belegebox.de>
2025-09-12 12:18:32 +0200

    init

---
+++ collection-map/collectionPointData.js
@@ -0,0 +1,15 @@
+var collectionPoints = [
+    {
+        "city": "Würzburg",
+        "postalCode": "97076",
+        "street": "Ohmstraße",
+        "house": "1",
+        "latLong": [49.7974, 9.9708],
+        "openingHours": {
+            "Sunday": ["12:00-18:00"]
+        },
+        "duration": "01.10 - 30.11",
+        "supportedOrganisations": ["RWC", "Samos Volunteers"],
+        "organizedBy": "HERMINE e. V."
+    }
+]
