65a0ef60c86bb2d934d17c9881e34de5b4f65c7d
Maik <maik.neubert@belegebox.de>
2025-10-13 19:57:32 +0200

    update data

--- data.js
+++ data.js
@@ -7,24 +7,14 @@ var cities = [
       {
         "name": "HERMINE e. V. Lagerhalle",
         "organization": "HERMINE e. V.",
-        "address": {
-          "street": "Ohmstraße",
-          "house": "1",
-          "postalCode": "97076"
-        },
+        "address": { "street": "Ohmstraße", "house": "1", "postalCode": "97076" },
         "latLong": [49.7974, 9.9708],
-        "openingHours": [
-          { "day": "Sonntag", "times": ["12:00-18:00"] }
-        ]
+        "openingHours": [{ "day": "Sonntag", "times": ["12:00-18:00"] }]
       },
       {
         "name": "KHG",
         "organization": "HERMINE e. V.",
-        "address": {
-          "street": "Hofstallstr.",
-          "house": "4",
-          "postalCode": "97070"
-        },
+        "address": { "street": "Hofstallstr.", "house": "4", "postalCode": "97070" },
         "latLong": [49.7951, 9.9405],
         "openingHours": [
           { "day": "Montag", "times": ["09:00-12:00", "14:00-16:00"] },
@@ -57,130 +47,7 @@ var cities = [
         "organization": "HERMINE",
         "address": { "street": "Fred-Joseph-Platz", "house": "3", "postalCode": "97082" },
         "latLong": [49.78963, 9.92469],
-        "openingHours": [
-          { "day": "Montag-Donnerstag", "times": ["15:00-21:00"] }
-        ]
-      }
-    ]
-  },
-  {
-    "name": "Dortmund",
-    "url": "dortmund",
-    "latLong": [51.53, 7.46],
-    "collectionPoints": [
-      {
-        "name": "Grenzenlose Wärme e. V.",
-        "organization": "Grenzenlose Wärme e. V.",
-        "address": {
-          "street": "Kanalstrasse 25, Halle 51",
-          "house": "25",
-          "postalCode": "44147"
-        },
-        "latLong": [51.529124, 7.440845],
-        "openingHours": [
-          { "day": "Donnerstag", "times": ["17:00-20:00"] },
-          { "day": "Auf Anfrage", "times": [] }
-        ]
-      }
-    ]
-  },
-  {
-    "name": "Biesenthal",
-    "url": "biesenthal",
-    "latLong": [52.76, 13.64],
-    "collectionPoints": [
-      {
-        "name": "Wir packen's an e. V.",
-        "organization": "Wir packen's an e. V.",
-        "address": {
-          "street": "Bahnhofstr.",
-          "house": "81 a",
-          "postalCode": "16359"
-        },
-        "latLong": [52.754285, 13.674776],
-        "openingHours": [
-          { "day": "Montag-Sonntag", "times": ["0-24"] },
-        ]
-      }
-    ]
-  },
-  {
-    "name": "Leipzig",
-    "url": "leipzig",
-    "latLong": [51.3310, 12.3730],
-    "collectionPoints": [
-      {
-        "name": "Cafe Später Vogel",
-        "organization": "Project Elpida",
-        "address": {
-          "street": "Kolonnadenstraße",
-          "house": "5/7",
-          "postalCode": "04109"
-        },
-        "latLong": [51.337984, 12.367241],
-        "openingHours": [
-          { "day": "Montag-Freitag", "times": ["10:00-20:00"] },
-          { "day": "Samstag-Sonntag", "times": ["09:00-20:00"] }
-        ]
-      },
-      {
-        "name": "Lixer",
-        "organization": "Project Elpida",
-        "address": {
-          "street": "Pörstener Str.",
-          "house": "9",
-          "postalCode": "04229"
-        },
-        "latLong": [51.312987, 12.323896],
-        "openingHours": [
-          { "day": "Öffnungszeiten", "times": ["<a href=\"https://zschocher.com/veranstaltungen/kalender\">Website</a>"] }
-        ]
-      },
-      {
-        "name": "Cafe Drei der Münze",
-        "organization": "Project Elpida",
-        "address": {
-          "street": "Münzgasse",
-          "house": "28",
-          "postalCode": "04107"
-        },
-        "latLong": [51.331687, 12.372144],
-        "openingHours": [
-          { "day": "Dienstag", "times": ["11:00-18:00"] },
-          { "day": "Mittwoch-Freitag", "times": ["11:00-22:00"] },
-          { "day": "Samstag", "times": ["12:00-00:00"] }
-        ]
-      },
-      {
-        "name": "Patina 45",
-        "organization": "Project Elpida",
-        "address": { "street": "Rabet", "house": "15", "postalCode": "04315" },
-        "latLong": [51.34340, 12.40256],
-        "openingHours": [
-          { "day": "Mo-Do", "times": ["16:00-22:00"] },
-          { "day": "Fr", "times": ["16:00-00:00"] },
-          { "day": "Sa-So", "times": ["14:00-22:00"] }
-        ]
-      },
-      {
-        "name": "Simmini",
-        "organization": "Project Elpida",
-        "address": { "street": "Eisenbahnstraße", "house": "109", "postalCode": "04315" },
-        "latLong": [51.34540, 12.41130],
-        "openingHours": [
-          { "day": "Sonntag-Dienstag", "times": ["12:00-19:00"] },
-          { "day": "Donnerstag-Samstag", "times": ["12:00-00:00"] }
-        ]
-      },
-      {
-        "name": "Vleischerei",
-        "organization": "Project Elpida",
-        "address": { "street": "Eisenbahnstraße", "house": "128", "postalCode": "04315" },
-        "latLong": [51.34505, 12.41243],
-        "openingHours": [
-          { "day": "Dienstag-Freitag", "times": ["17:00-22:00"] },
-          { "day": "Samstag-Sonntag", "times": ["12:00-22:00"] }
-        ]
+        "openingHours": [{ "day": "Montag-Donnerstag", "times": ["15:00-21:00"] }]
       }
     ]
   },
@@ -192,11 +59,7 @@ var cities = [
       {
         "name": "SG GM/BTSV 1850 Heimspiele Oktober / November",
         "organization": "Project Elpida",
-        "address": {
-          "street": "Siemensstraße",
-          "house": "20 A",
-          "postalCode": "10551"
-        },
+        "address": { "street": "Siemensstraße", "house": "20 A", "postalCode": "10551" },
         "latLong": [52.533355, 13.338357],
         "openingHours": [
           { "day": "4.10.", "times": [] },
@@ -219,94 +82,164 @@ var cities = [
         "openingHours": [{ "day": "Mo-Do", "times": ["11:00-19:00"] }]
       },
       {
-        "name": "AWO Begegnungszentrum",
-        "organization": "Project Elpida",
-        "address": { "street": "Adalbertstraße", "house": "23a", "postalCode": "10997" },
-        "latLong": [52.50379, 13.42148],
-        "openingHours": [{ "day": "Mo-Fr", "times": ["09:00-15:00"] }]
+        "name": "Kukumu",
+        "organization": "Seebrücke Berlin",
+        "address": { "street": "Lübeckerstraße", "house": "43", "postalCode": "10559" },
+        "latLong": [52.5280091, 13.3452198],
+        "openingHours": [{ "day": "verschiedene Öffnungszeiten", "times": [] }]
       },
       {
-        "name": "Bibliothek Tiergarten-Süd",
-        "organization": "Project Elpida",
-        "address": { "street": "Lützowstraße", "house": "27", "postalCode": "10785" },
-        "latLong": [52.50375, 13.36342],
-        "openingHours": [{ "day": "Mo-Fr", "times": ["12:30-18:00"] }]
+        "name": "Stiftung Nord-Süd-Brücken",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Greifswalder Strasse", "house": "33a", "postalCode": "10405" },
+        "latLong": [52.5332818, 13.4301837],
+        "openingHours": [{ "day": "Montag-Freitag", "times": ["10:00-15:00"] }]
+      },
+      {
+        "name": "Familienzentrum der Sozdia",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Pfarrstr.", "house": "93", "postalCode": "10317" },
+        "latLong": [52.505928, 13.478261],
+        "openingHours": [{ "day": "Montag-Donnerstag", "times": ["10:00-18:00"] }]
+      },
+      {
+        "name": "Inkota",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Chrysanthemenstr.", "house": "1-3", "postalCode": "10497" },
+        "latLong": [48.1095201, 11.4882983],
+        "openingHours": [{ "day": "Montag-Freitag", "times": ["10:00-16:00"] }]
       }
     ]
   },
   {
-    "name": "Vaihingen an der Enz",
-    "url": "vaihingen-enz",
-    "latLong": [48.933, 8.957],
+    "name": "Nürnberg",
+    "url": "nuernberg",
+    "latLong": [49.4561777, 11.0307245],
     "collectionPoints": [
       {
-        "name": "AK Asyl Vaihingen an der Enz",
-        "organization": "AK Asyl Vaihingen/E",
-        "address": {
-          "street": "Friedrichstr.",
-          "house": "3",
-          "postalCode": "71665"
-        },
-        "latLong": [48.93451, 8.957622],
+        "name": "Heizhaus",
+        "organization": "Seebrücke Nürnberg, Seaeye Nürnberg",
+        "address": { "street": "Wandererstraße", "house": "89", "postalCode": "90431" },
+        "latLong": [49.4561777, 11.0307245],
+        "openingHours": [{ "day": "22.11.2025", "times": ["11:00-17:00"] }]
+      }
+    ]
+  },
+  {
+    "name": "Bad Freienwalde",
+    "url": "bad-freienwalde",
+    "latLong": [52.7780753, 14.0511898],
+    "collectionPoints": [
+      {
+        "name": "Dekoscheune",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Frankfurter Str. Ausbau", "house": "24", "postalCode": "16259" },
+        "latLong": [52.7780753, 14.0511898],
+        "openingHours": [{ "day": "Montag-Sonntag", "times": ["10:00-18:00"] }]
+      }
+    ]
+  },
+  {
+    "name": "Eberswalde",
+    "url": "eberswalde",
+    "latLong": [52.8309823, 13.8083155],
+    "collectionPoints": [
+      {
+        "name": "Eckladen Tante Soeren",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Rudolf-Breitscheid-Str.", "house": "16", "postalCode": "16225" },
+        "latLong": [52.8309823, 13.8083155],
+        "openingHours": [{ "day": "Dienstag, Donnerstag-Samstag", "times": ["12:00-20:00"] }]
+      }
+    ]
+  },
+  {
+    "name": "Zossen OT Wünsdorf",
+    "url": "zossen-wuensdorf",
+    "latLong": [52.1641888, 13.4759096],
+    "collectionPoints": [
+      {
+        "name": "Basar für alle (Helferkreis der Stadt Zossen)",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Am Bürgerhaus", "house": "1", "postalCode": "15806" },
+        "latLong": [52.1641888, 13.4759096],
         "openingHours": [
-          { "day": "Freitag 10.10.", "times": ["14:00-18:00"] },
-          { "day": "Samstag 11.10.", "times": ["11:00-17:00"] }
+          { "day": "Montag", "times": ["13:00-15:00"] },
+          { "day": "Mittwoch", "times": ["16:00-18:00"] }
         ]
       }
     ]
   },
   {
-    "name": "Köln",
-    "url": "koeln",
-    "latLong": [50.9381, 6.96],
+    "name": "Rehagen",
+    "url": "rehagen",
+    "latLong": [52.165886, 13.3811285],
     "collectionPoints": [
       {
-        "name": "Cologne Cares e. V.",
-        "organization": "Cologne Cares e. V.",
-        "address": {
-          "street": "Neusser Platz",
-          "house": "8",
-          "postalCode": "50670"
-        },
-        "latLong": [50.954564, 6.95797],
-        "openingHours": [
-          { "day": "Sonntag 26.10.", "times": ["13:00-16:00"] }
-        ]
+        "name": "Dies & Das",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Am Bahnhof", "house": "1", "postalCode": "15806" },
+        "latLong": [52.165886, 13.3811285],
+        "openingHours": [{ "day": "Dienstag & Donnerstag", "times": ["14:00-17:00"] }]
       }
     ]
   },
   {
-    "name": "Herzogenaurach",
-    "url": "herzogenaurach",
-    "latLong": [49.567, 10.885],
+    "name": "Müncheberg",
+    "url": "muencheberg",
+    "latLong": [52.50341, 14.15228],
     "collectionPoints": [
       {
-        "name": "Herzo Packt an e.V. Scheune",
-        "organization": "Herzo Packt an e.V.",
-        "address": { "street": "Eichelmühlgasse", "house": "24", "postalCode": "91074" },
-        "latLong": [49.566, 10.89332],
-        "openingHours": []
+        "name": "Spiel Ahoi",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Bergmannstr.", "house": "9", "postalCode": "15374" },
+        "latLong": [52.50341, 14.15228],
+        "openingHours": [{ "day": "Täglich", "times": ["08:00-20:00"] }]
+      }
+    ]
+  },
+  {
+    "name": "Horstedt",
+    "url": "horstedt",
+    "latLong": [53.177, 9.201],
+    "collectionPoints": [
+      {
+        "name": "ARIES Umweltprodukte",
+        "organization": "Wir packen's an e. V.",
+        "address": { "street": "Stapeler Dorfstraße", "house": "23", "postalCode": "27367" },
+        "latLong": [53.177666, 9.201367],
+        "openingHours": [{ "day": "Montag-Freitag", "times": ["08:00-16:00"] }]
       }
     ]
   }
 ];
 
 
+
 var organizations = [
   {
     "name": "HERMINE e. V.",
     "cities": ["Würzburg"],
-    "supportedOrganisations": ["RWC", "SV", "CA", "JNRC"],
+    "supportedOrganisations": ["RWC", "SV", "CA", "JNRC"]
   },
   {
     "name": "Grenzenlose Wärme e. V.",
     "cities": ["Dortmund"],
-    "supportedOrganisations": ["RWC", "MRS"],
+    "supportedOrganisations": ["RWC", "MRS"]
   },
   {
     "name": "Wir packen's an e. V.",
-    "cities": ["Biesenthal"],
-    "supportedOrganisations": ["RWC", "SV", "JA", "CA", "NNK", "MRS"],
+    "cities": [
+      "Biesenthal",
+      "Berlin",
+      "Bad Freienwalde",
+      "Eberswalde",
+      "Zossen OT Wünsdorf",
+      "Rehagen",
+      "Müncheberg",
+      "Horstedt"
+    ],
+    "supportedOrganisations": ["RWC", "SV", "JA", "CA", "NNK", "MRS"]
   },
   {
     "name": "Project Elpida",
@@ -323,7 +256,23 @@ var organizations = [
     "cities": ["Köln"],
     "supportedOrganisations": ["SV"]
   },
-]
+  {
+    "name": "Seebrücke Berlin",
+    "cities": ["Berlin"],
+    "supportedOrganisations": []
+  },
+  {
+    "name": "Seebrücke Nürnberg",
+    "cities": ["Nürnberg"],
+    "supportedOrganisations": []
+  },
+  {
+    "name": "Seaeye Nürnberg",
+    "cities": ["Nürnberg"],
+    "supportedOrganisations": []
+  }
+];
+
 
 var supportedOrganisations = [
   {
